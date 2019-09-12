dd71574426a05834cb33d14402f9040005442130
study91 <1964073@qq.com>
2019-09-12 15:42:02 +0800

    no message

---
+++ app/src/main/java/com/study91/audiobook/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.study91.audiobook.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.study91.audiobook.R;
+
+/**
+ * 主活动
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
