6cce9278b27d27404f22cca983c4757517698101
study91 <1964073@qq.com>
2019-11-15 12:33:17 +0800

    no message

---
+++ app/src/main/java/com/study91/audiobook/ad/AdManager.java
@@ -0,0 +1,7 @@
+package com.study91.audiobook.ad;
+
+/**
+ * 广告管理器
+ */
+public class AdManager {
+}
---
+++ app/src/main/java/com/study91/audiobook/ad/IAd.java
@@ -0,0 +1,7 @@
+package com.study91.audiobook.ad;
+
+/**
+ * 广告接口
+ */
+public interface IAd {
+}
